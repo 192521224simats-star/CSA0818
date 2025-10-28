9e3adfca03eb391b7e39959c04d7ef6745fc14a3
192521224simats-star <192521224.simats@saveetha.com>
2025-10-28 14:06:11 +0530

    Add files via upload

---
+++ Simple programs.py
@@ -0,0 +1,33 @@
+# Area of the circle
+a=int(input('Enter the radius:'))
+pi=3.14
+area=3.14*a**2
+print('The area of the circle:', area)
+
+#Swap number
+a=int(input('Enter the First Number a:'))
+b=int(input('Enter the Second Number b:'))
+a,b=b,a
+print('The swaped number is a,b:' ,a,b)
+
+# squareroot of the number
+a=int(input('Enter the number:'))
+sr=a**(1/2)
+print('The square root of the number is:',sr)
+
+#Cuberoot of the number
+a=int(input('Enter the number:'))
+cr=a**(1/3)
+print('The cuberoot of the number:',cr)
+
+#celsuis to fahrenheit
+cel=float(input('Enter the celsuis value:'))
+f=(cel*5/9)+32
+print('The Fahrenheit is:',f)
+
+#Fahrenheit to celsuis
+f=float(input('Enter tbe fahrenheit value:'))
+cel=(f-32)*95
+print('The celsuis is :',cel)
+
+
